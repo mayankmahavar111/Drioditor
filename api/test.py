c9ae86a3bea6da7b172a4644bcdb077a6d557091
mayankmahavar111 <mayankmahavar111@gmail.com>
2018-10-16 21:34:19 +0530

    notepad

---
+++ api/test.py
@@ -0,0 +1,12 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+import pytesseract
+
+# If you don't have tesseract executable in your PATH, include the following:
+#pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
+# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+
+# Simple image to string
+print(pytesseract.image_to_string(Image.open('im.png')))
