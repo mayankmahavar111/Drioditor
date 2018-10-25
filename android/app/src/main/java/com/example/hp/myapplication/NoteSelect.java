f26cb38eeed02024699762a1b7ff1d4f0329b100
mayankmahavar111 <mayankmahavar111@gmail.com>
2018-10-25 19:11:22 +0530

    note

---
+++ android/app/src/main/java/com/example/hp/myapplication/NoteSelect.java
@@ -0,0 +1,93 @@
+package com.example.hp.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.hp.myapplication.R;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class NoteSelect extends AppCompatActivity {
+
+    private List <NoteBuilder> notesList = new ArrayList <>();
+    private NotesAdapter nAdapter;
+    private RecyclerView notesRecycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_select);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        notesRecycler = (RecyclerView) findViewById(R.id.notes);
+
+        nAdapter = new NotesAdapter(notesList);
+        RecyclerView.LayoutManager mLayoutManager =
+                new LinearLayoutManager(getApplicationContext());
+        notesRecycler.setLayoutManager(mLayoutManager);
+        notesRecycler.setItemAnimator(new DefaultItemAnimator());
+        notesRecycler.setAdapter(nAdapter);
+
+        prepareNotes();
+
+    }
+
+    private void prepareNotes() {
+        File directory;
+        directory = getFilesDir();
+        File[] files = directory.listFiles();
+        String theFile;
+        for (int f = 1; f <= files.length; f++) {
+            theFile = "Note" + f + ".txt";
+            NoteBuilder note = new NoteBuilder(theFile, Open(theFile));
+            notesList.add(note);
+        }
+
+    }
+
+    public String Open(String fileName) {
+        String content = "";
+        try {
+            InputStream in = openFileInput(fileName);
+            if ( in != null) {
+                InputStreamReader tmp = new InputStreamReader( in );
+                BufferedReader reader = new BufferedReader(tmp);
+                String str;
+                StringBuilder buf = new StringBuilder();
+                while ((str = reader.readLine()) != null) {
+                    buf.append(str + "\n");
+                } in .close();
+
+                content = buf.toString();
+            }
+        } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        return content;
+    }
+}
---
+++ note/app/src/main/java/com/example/hp/note/NotesBuilder.java
@@ -0,0 +1,23 @@
+package com.example.hp.note;
+
+public class NotesBuilder {
+
+    private String title,
+            content;
+
+    public NotesBuilder() {
+    }
+
+    public NotesBuilder(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
