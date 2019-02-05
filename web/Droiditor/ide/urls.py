0826063e06a4071cdca27cf7217c603d12960ccc
ManoharMadanu <manoharmadanu@gmail.com>
2019-02-05 10:54:47 +0530

    Manohar got form details

--- web/Droiditor/ide/urls.py
+++ web/Droiditor/ide/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('ml_recommendation/', views.ml_recommendation_view),
+    path('ml_recommendation/', views.ml_recommendation_view, name="ml_recommendation"),
+    path('ml_recommendation/run', views.ml_recommendation_run, name="ml_recommendation_run"),
 ]
--- web/Droiditor/ide/views.py
+++ web/Droiditor/ide/views.py
@@ -1,9 +1,16 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 
 # Create your views here.
 
 def ml_recommendation_view(request):
     template = 'ide/ml_recommendation_form.html'
     return render(request, template)
+
+def ml_recommendation_run(request):
+    if request.method == 'POST':
+        drive_url=request.POST.get('drive_url')
+        dataset_name=request.POST.get('dataset_name')
+        email=request.POST.get('email')
+        pre_process_code=request.POST.get('pre_process_code')
+        print (drive_url+'\n'+dataset_name+'\n'+email)
+        return redirect('ml_recommendation')
