af34c0a243682d2ccd0fe20cdccd7b9e5e0e527a
ManoharMadanu <manoharmadanu@gmail.com>
2019-02-05 23:36:43 +0530

    Manohar template frontend
    Frontend for template

--- web/Droiditor/ide/urls.py
+++ web/Droiditor/ide/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('ml_recommendation/', views.ml_recommendation_view, name="ml_recommendation"),
     path('ml_recommendation/run', views.ml_recommendation_run, name="ml_recommendation_run"),
+    path('ml_template/', views.ml_template, name="ml_template"),
+    path('ml_template/run', views.ml_template_run, name="ml_template_run"),
 ]
--- web/Droiditor/ide/views.py
+++ web/Droiditor/ide/views.py
@@ -14,3 +14,16 @@ def ml_recommendation_run(request):
         pre_process_code=request.POST.get('pre_process_code')
         print (drive_url+'\n'+dataset_name+'\n'+email)
         return redirect('ml_recommendation')
+
+def ml_template(request):
+    template = 'ide/ml_template.html'
+    return render(request, template)
+
+def ml_template_run(request):
+    if request.method == 'POST':
+        drive_url=request.POST.get('drive_url')
+        dataset_name=request.POST.get('dataset_name')
+        email=request.POST.get('email')
+        pre_process_code=request.POST.get('pre_process_code')
+        print (drive_url+'\n'+dataset_name+'\n'+email+'\n'+pre_process_code)
+        return redirect('ml_template')
