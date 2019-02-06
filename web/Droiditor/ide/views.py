fe1c0f91be58a58d589f9508073d263669e19c5a
ManoharMadanu <manoharmadanu@gmail.com>
2019-02-06 22:49:15 +0530

    Manohar fixed

--- web/Droiditor/ide/views.py
+++ web/Droiditor/ide/views.py
@@ -29,10 +29,6 @@ def main_page(request):
     return render(request,template)
 
 
-def ml_recommendation_view(request):
-    template = 'ide/ml_recommendation_form.html'
-    return render(request, template)
-
 def ml_recommendation_run(request):
     if request.method == 'POST':
         drive_url=request.POST.get('drive_url')
@@ -41,17 +37,24 @@ def ml_recommendation_run(request):
         pre_process_code=request.POST.get('pre_process_code')
         print (drive_url+'\n'+dataset_name+'\n'+email)
         return redirect('ml_recommendation')
+    else:
+        template = 'ide/ml_recommendation_form.html'
+        return render(request, template)
 
 def ml_template(request):
     template = 'ide/ml_template.html'
     return render(request, template)
 
 def ml_template_run(request):
+    template = 'ide/ml_recommendation_form.html'
     if request.method == 'POST':
         drive_url=request.POST.get('drive_url')
         dataset_name=request.POST.get('dataset_name')
         email=request.POST.get('email')
         pre_process_code=request.POST.get('pre_process_code')
-        print (drive_url+'\n'+dataset_name+'\n'+email+'\n'+pre_process_code)
-        postRequest(url=drive_url,name=dataset_name,email=email,code=pre_process_code)
-        return redirect('ml_template')
+        request.session['drive_url']=drive_url
+        request.session['dataset_name'] =dataset_name
+        request.session['email'] =email
+        request.session['pre_process_code'] =pre_process_code
+        #postRequest(url=drive_url,name=dataset_name,email=email,code=pre_process_code)
+        return redirect('ml_template',drive_url="google.com")
